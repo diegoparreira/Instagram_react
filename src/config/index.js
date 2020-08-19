b93b3e3ec59be7dcb8cfe3d4a7ea2db02bbc7048
Diego Parreira <diego.parreira94@gmail.com>
2020-08-19 16:01:27 -0300

    Correção server

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 const URL = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://instagram-api.herokuapp.com';
+  : 'https://instagram-react-api.herokuapp.com';
 
 export default { URL };
